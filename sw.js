4f4117ddadf217baee4e4a6f8cdbf4cf9f6be58e
forssenm <forssenm@gmail.com>
2018-02-20 14:10:19 +0100

    Initial commit

---
+++ sw.js
@@ -0,0 +1,46 @@
+var CACHE_NAME = 'cache-v1';
+var urlsToCache = [
+    './',
+    './styles/main.css',
+    './scripts/main.js',
+    './content/squeed.jpg'
+];
+
+self.addEventListener('install', function (event) {
+    //Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    )
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                //Cache hit - return promise
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    )
+});
+
+self.addEventListener('activate', function (event) {
+    var cacheWhiteList = ['cache-v1'];
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheWhiteList.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        })
+    )
+});
